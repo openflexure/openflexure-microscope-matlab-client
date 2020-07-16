e5ad50aefae2a830b10f3ab1877ec51a557c589e
Samuel McDermott <sjm263@cam.ac.uk>
2020-07-16 23:38:16 +0100

    Rename class and start docs

--- OFMClient.m
+++ OFMClient.m
@@ -1,7 +1,33 @@
-classdef MicroscopeClient <handle
-    %MicroscopeClient Simple client code for the OpenFlexure Microscope in
-    %Matlab
-    %   Detailed explanation goes here
+classdef OFMClient < handle
+    %OFMClient Simple client code for the OpenFlexure Microscope in
+    %MATLAB
+    %   This MATLAB class makes it easy to connect and control an
+    %   OpenFlexure Microscope over a network. You are able to move the
+    %   microscope and get the images from the microscope, as well as run
+    %   extensions.
+    %
+    %OFMClient Properties:
+    %   host - The microscope's hostname or IP address
+    %   port - The microscope's port
+    %   extensions - A struct of the microscope's currently loaded
+    %   extensions.
+    %
+    %OFMClient Methods:
+    %   base_uri - Return the microscope base URI.
+    %   get_json - Make an HTTP GET request and return the response.
+    %   post_json - Make an HTTP POST request and return the response.
+    %   populate_extensions - Get a struct of the extensions and store in
+    %   obj.extensions.
+    %   position - Return the position of the stage as a struct.
+    %   position_as_matrix - Return the position of the stage as a matrix.
+    %   move - Move the stage to a given position.
+    %   move_rel - Move the stage by a given amount.
+    %   query_background_task - Request the status of a background task.
+    %   capture_image - Capture an image and return it (TODO)
+    %   grab_image - Grab an image from the stream and return it.
+    %   calibrate_xy - Move the stage in X and Y to calibrate stage
+    %   movements vs camera movements.
+    %   autofocus - Run the fast autofocus routine.
     
     properties (SetAccess = protected)
         host
@@ -10,9 +36,9 @@ classdef MicroscopeClient <handle
     end
     
     methods
-        function obj = MicroscopeClient(varargin)
-            %MicroscopeClient Construct an instance of this class
-            %   Detailed explanation goes here
+        function obj = OFMClient(varargin)
+            %OFMClient Construct an instance of this class
+            %   
             defaultPort = '5000';
             p = inputParser;
             addRequired(p,'host',@(s) ischar(s) || isstring(s));
@@ -23,16 +49,26 @@ classdef MicroscopeClient <handle
             disp('Connecting to microscope:') 
             fprintf('<a href = "http://%s:%s">%s:%s</a>\n',obj.host,obj.port,obj.host,obj.port);
             obj.populate_extensions()
-          
         end
         
         function outputArg = base_uri(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+            %base_uri Return the microscope base URI.
+            %
             outputArg = sprintf('http://%s:%s/api/v2', obj.host,obj.port);
         end
-
+        
+        function outputArg = get_json(obj, path)
+            %get_json Make an HTTP GET request and return the response.
+            if ~startsWith(path,'http')
+                path = [obj.base_uri() path];
+            end
+            options = weboptions('Timeout',30);
+            outputArg =  webread(path, options);         
+        end
+        
         function outputArg = post_json(obj, path,varargin)
+            %post_json Make an HTTP POST request and return the response.
+            %
             defaultPayload = struct;
             defaultWaitOnTask = 'auto'; 
             p = inputParser;
@@ -59,18 +95,10 @@ classdef MicroscopeClient <handle
             end
             
         end
-        
-        function outputArg = get_json(obj, path)
-            if ~startsWith(path,'http')
-                path = [obj.base_uri() path];
-            end
-            options = weboptions('Timeout',30);
-            outputArg =  webread(path, options);         
-        end
-        
- 
             
         function populate_extensions(obj)
+            %populate_extensions Get a struct of the extensions and store in obj.extensions.
+            %
             extensions_struct  = obj.get_json('/extensions/');
             for i = 1: numel(extensions_struct)
                 title = extensions_struct(i).title;
@@ -79,15 +107,19 @@ classdef MicroscopeClient <handle
         end
         
         function outputArg = position(obj)
+            %position Return the position of the stage as a struct.
             outputArg = obj.get_json('/instrument/state/stage/position');
         end
 
         function outputArg = position_as_matrix(obj)
+            %position_as_matrix Return the position of the stage as a matrix.
+            %
             pos = obj.position();
             outputArg = [pos.x, pos.y, pos.z];
         end
         
         function move(obj,position, varargin)
+            %move Move the stage to a given position.
             defaultAbsolute = true;
             p = inputParser;
             addOptional(p,'absolute',defaultAbsolute,@(s) islogical(s));
@@ -106,14 +138,21 @@ classdef MicroscopeClient <handle
         end
         
         function move_rel(obj, position)
+            %move_rel Move the stage by a given amount.
+            %
             obj.move(position,false);
         end
         
         function outputArg = query_background_task(obj, task)
+            %query_background_task Request the status of a background task.
+            %
             outputArg = obj.get_json(task.links.self.href);
         end
         
         function outputArg = capture_image(obj)
+            %capture_image Capture an image and return it (TODO)
+            %            
+            
             %TODO
             %payload.use_video_port = true;
             %payload.bayer = false;
@@ -122,22 +161,24 @@ classdef MicroscopeClient <handle
         end
         
         function outputArg = grab_image(obj)
+            %grab_image Grab an image from the stream and return it.
+            %
             outputArg =  obj.get_json('/streams/snapshot');
         end
         
         function outputArg = calibrate_xy(obj)
+            %calibrate_xy Move the stage in X and Y to calibrate stage movements vs camera movements.
+            %
             outputArg = obj.extensions.org_DOT_openflexure_DOT_camera_stage_mapping.calibrate_xy.post_json();
         end
         
         function autofocus(obj)
+            %autofocus Run the fast autofocus routine.
             obj.extensions.org_DOT_openflexure_DOT_autofocus.fast_autofocus.post_json();
         end    
     end
 end
 
-function outputArg = replace_dots(text)
-    outputArg = replace(text,'.','_DOT_');
-end  
 
 
 
