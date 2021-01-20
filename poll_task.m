bc0029d045f723f488a00e1101e60f7726711a3d
Samuel McDermott <sjm263@cam.ac.uk>
2021-01-20 23:10:02 +0000

    Fix task polling and output logs from microscope

--- poll_task.m
+++ poll_task.m
@@ -1,15 +1,25 @@
 function outputArg = poll_task(task)
     %poll_task Get the status of a task running on the microscope.
-    assert(is_a_task(task),'poll_task must be called with a struct representation of a task');
-    while contains(task.status,['running','idle'])
-        disp('here');
+    assert(is_a_task(task),'poll_task must be called with a struct representation of a task');        
+    log_n=1;
+    while (contains(task.status,'running') || contains(task.status,'idle'))
         options = weboptions('Timeout',30);
         task =  webread(task_href(task), options);
+            while length(task.log) >log_n
+                d = task.log(log_n);
+                fprintf('%s: %s\n' , d.levelname, d.message);
+                log_n = log_n +1;
+            end
     end
+
     try
-        outputArg = task.return;
+        outputArg = task.output;
     catch
-        warning('Task endpoint was missing a return value.');
-        outputArg = {};
+        try 
+            outputArg = task.return;
+        catch
+            warning('Task endpoint was missing a return value.');
+            outputArg = {};
+        end
     end
  end
