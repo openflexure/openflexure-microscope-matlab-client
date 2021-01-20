7b196dbaa11955aaf3f492da3961f3adde29bec5
Samuel McDermott <sjm263@cam.ac.uk>
2021-01-20 23:10:19 +0000

    Update the requirements to be a task

--- is_a_task.m
+++ is_a_task.m
@@ -1,4 +1,4 @@
 function outputArg = is_a_task(task)
     %is_a_task Returns whether a reponse struct is a task.
-    outputArg =  contains(task_href(task),'/api/v2/tasks/');
+    outputArg =  contains(task_href(task),'/api/v2/tasks/')| contains(task_href(task),'/api/v2/actions/')  ;
 end
